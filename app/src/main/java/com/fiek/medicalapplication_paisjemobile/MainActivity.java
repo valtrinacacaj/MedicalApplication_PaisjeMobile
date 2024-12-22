02d7613f9b3143e3c6314a4fc4132679ae322874
valtrinacacaj <valtrina.cacaj@student.uni-pr.edu>
2024-12-22 20:00:35 +0100

    Initial commit

---
+++ app/src/main/java/com/fiek/medicalapplication_paisjemobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fiek.medicalapplication_paisjemobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
