700905901920f963136f0cb069f005b57ff32406
valtrinacacaj <valtrina.cacaj@student.uni-pr.edu>
2024-12-22 23:22:45 +0100

    DatabaseHelper added

---
+++ app/src/main/java/com/fiek/medicalapplication_paisjemobile/DatabaseHelper.java
@@ -0,0 +1,62 @@
+package com.fiek.medicalapplication_paisjemobile;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String databaseName = "MedicalApp.db";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context,"MedicalApp.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase MyDatabase) {
+    MyDatabase.execSQL("create table allusers(email TEXT primary key, password TEXT)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase MyDatabase, int oldVersion, int newVersion) {
+    MyDatabase.execSQL("drop Table if exists allusers");
+    }
+
+    public Boolean insertData(String email, String password){
+        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email", email);
+        contentValues.put("password", password);
+        long result = MyDatabase.insert("users", null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Boolean checkEmail(String email){
+        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+        Cursor cursor = MyDatabase.rawQuery("Select * from users where email = ?", new String[]{email});
+        if(cursor.getCount() > 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public Boolean checkEmailPassword(String email, String password){
+        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+        Cursor cursor = MyDatabase.rawQuery("Select * from users where email = ? and password = ?", new String[]{email, password});
+        if (cursor.getCount() > 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
