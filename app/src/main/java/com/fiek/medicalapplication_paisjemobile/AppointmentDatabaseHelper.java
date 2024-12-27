2116445b814ed8a49f9d991b411c16345e0ad2a4
VleraDerplanin <vlera.derplanin@student.uni-pr.edu>
2024-12-27 03:33:04 +0100

    Ndryshime ne pjesen e HomePage

---
+++ app/src/main/java/com/fiek/medicalapplication_paisjemobile/AppointmentDatabaseHelper.java
@@ -0,0 +1,30 @@
+//package com.fiek.medicalapplication_paisjemobile;
+//
+//public class AppointmentDatabaseHelper {
+//    private String dataTitle;
+//    private String dataDesc;
+//    private String dataDate;
+//
+//    // Konstruktor për inicializimin e fushave
+//    public AppointmentDatabaseHelper(String dataTitle, String dataDesc, String dataDate) {
+//        this.dataTitle = dataTitle;
+//        this.dataDesc = dataDesc;
+//        this.dataDate = dataDate;
+//    }
+//
+//    // Getter për dataTitle
+//    public String getDataTitle() {
+//        return dataTitle;
+//    }
+//
+//    // Getter për dataDesc
+//    public String getDataDesc() {
+//        return dataDesc;
+//    }
+//
+//    // Getter për dataDate
+//    public String getDataDate() {
+//        return dataDate;
+//    }
+//}
+//
---
+++ app/src/main/java/com/fiek/medicalapplication_paisjemobile/UploadActivity.java
@@ -0,0 +1,24 @@
+package com.fiek.medicalapplication_paisjemobile;
+
+import android.os.Bundle;
+import android.widget.Button;
+//import android.widget.EditText;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class UploadActivity extends AppCompatActivity {
+//    Button saveButton;
+//    EditText uploadAppointment ,uploadDesc,uploadDate;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_upload);
+//
+//        uploadAppointment = findViewById(R.id.uploadAppointment);
+//        uploadDesc = findViewById(R.id.uploadDesc);
+//        uploadDate = findViewById(R.id.uploadDate);
+//        saveButton = findViewById(R.id.saveButton);
+//    }
+//
+//        // Add any additional logic here
+//}
