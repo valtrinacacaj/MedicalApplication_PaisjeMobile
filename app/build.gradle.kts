bd91644b1bdf646b9f71ed86e692784c79878eb7
valtrinacacaj <valtrina.cacaj@student.uni-pr.edu>
2024-12-30 16:47:26 +0100

    Forgot password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -52,6 +53,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("com.google.firebase:firebase-auth:23.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/com/fiek/medicalapplication_paisjemobile/LogInActivity.java
+++ app/src/main/java/com/fiek/medicalapplication_paisjemobile/LogInActivity.java
@@ -3,15 +3,30 @@ package com.fiek.medicalapplication_paisjemobile;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.fiek.medicalapplication_paisjemobile.databinding.ActivityLogInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity {
 
     ActivityLogInBinding binding;
     DatabaseHelper databaseHelper;
+
+    private FirebaseAuth mAuth;
+
+    TextView forgotPasswordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +36,46 @@ public class LogInActivity extends AppCompatActivity {
 
         // Instanco DatabaseHelper
         databaseHelper = new DatabaseHelper(this);
+        mAuth = FirebaseAuth.getInstance();
 
+        forgotPasswordText = findViewById(R.id.forgotPasswordText);
+        forgotPasswordText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LogInActivity.this);
+                View dialogView = getLayoutInflater().inflate(R.layout.activity_forgot_password, null);
+                EditText emailInput = dialogView.findViewById(R.id.emailInput);
 
-        TextView forgotPasswordText = findViewById(R.id.forgotPasswordText);
-        forgotPasswordText.setOnClickListener(v -> {
-            Intent intent = new Intent(LogInActivity.this, ForgotPasswordActivity.class);
-            startActivity(intent);
+                builder.setView(dialogView);
+                AlertDialog dialog = builder.create();
+
+                dialogView.findViewById(R.id.requestPasswordResetButton).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String userEmail = emailInput.getText().toString();
+
+                        if (TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
+                            Toast.makeText(LogInActivity.this, "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        mAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LogInActivity.this, "Check your email", Toast.LENGTH_SHORT).show();
+                                    dialog.dismiss();
+                                } else {
+                                    Toast.makeText(LogInActivity.this, "Unable to send, failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+
+                });
+                dialog.show();
+            }
         });
+
         // Klikimi për Login
         binding.loginButton.setOnClickListener(v -> {
             String email = binding.loginEmail.getText().toString().trim();
@@ -46,7 +94,7 @@ public class LogInActivity extends AppCompatActivity {
                     editor.apply();
 
                     Toast.makeText(LogInActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LogInActivity.this,SplashActivity.class);
+                    Intent intent = new Intent(LogInActivity.this, SplashActivity.class);
                     startActivity(intent);
                     finish();
                 } else {
